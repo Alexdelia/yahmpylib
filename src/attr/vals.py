bde0a3bb909ae166ac8e879ea81e39ac0d5e882f
Alexdelia <alexandre.delille.57@gmail.com>
2023-02-07 12:20:58 +0100

    sort

--- src/attr/vals.py
+++ src/attr/vals.py
@@ -3,6 +3,11 @@ from typing import List, Union
 
 
 def vals(obj: Union[object, type]) -> List[str]:
+    """
+    returns a list of the values of the attributes of the given object or type
+
+    works with Enum types and instances
+    """
     if isinstance(obj, type):
         if issubclass(obj, Enum):
             return [e.value for e in obj]
